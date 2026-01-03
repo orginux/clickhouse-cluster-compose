c40992aee33b680506e76a8322b2dd996cb7541d
Gorobets Denis <orginux@protonmail.com>
2026-01-03 18:57:48 +0100

    complex config

--- schema/redpanda/events.sql
+++ schema/redpanda/events.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS redpanda ON CLUSTER '{cluster}'
 
 CREATE TABLE IF NOT EXISTS redpanda.events_local ON CLUSTER '{cluster}'
 (
-    timestamp   DateTime64,
+    timestamp   DateTime,
     id          UUID,
     value       UInt32
 )
@@ -14,4 +14,22 @@ ORDER BY (timestamp, id)
 CREATE TABLE IF NOT EXISTS redpanda.events ON CLUSTER '{cluster}'
         AS redpanda.events_local
 ENGINE = Distributed('{cluster}', redpanda, events_local)
+;
+
+CREATE TABLE IF NOT EXISTS redpanda.events_url ON CLUSTER '{cluster}'
+(
+    timestamp   DateTime,
+    id          UUID,
+    value       UInt32
+)
+ENGINE = URL('http://clickhouse-redpanda-connect:4195/events', 'JSONEachRow')
+;
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS redpanda.events_to_url_mv ON CLUSTER '{cluster}' TO redpanda.events_url
+AS SELECT
+    timestamp,
+    id,
+    value
+FROM redpanda.events_local
+WHERE (value % 2) = 0
 ;
